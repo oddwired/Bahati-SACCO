47e407b6f4c0ff5100cb361cf38f7d9d86e16058
Shem Kiptoo <shemkemboi@gmail.com>
2019-05-23 19:17:32 +0300

    Added Reports

--- resources/views/link_sent.blade.php
+++ resources/views/link_sent.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <h4>Link Sent</h4><br>
             <p>{{$message}}</p>
         </div>
     </div>
